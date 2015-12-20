88656d86e56a4e3fdef4d968b0126b4e4ee2a38e
jeremiah_lim <jeremiah-lim@outlook.com>
2015-12-21 06:16:58 +0800

    added front end form to submit and recieve message

--- public/js/app.js
+++ public/js/app.js
@@ -7,4 +7,16 @@ socket.on('connect',function () {
 socket.on('message',function (message) {
 	console.log('new message');
 	console.log(message.text);
+	$('#chat').prepend("<p>" + message.text + "</p>");
+});
+
+// handles submitting of new message
+var $form = $('#message-form');
+
+$form.on('submit',function (event) {
+	event.preventDefault();
+	socket.emit('message',{
+		text:$form.find('input[name=message]').val() 
+	});
+	$form.find('input[name=message]').val(''); 
 });
