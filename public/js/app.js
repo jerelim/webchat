289d9db608af9f2f95c5dc196e4de7752822f046
jeremiah_lim <jeremiah-lim@outlook.com>
2015-12-21 05:59:04 +0800

    added custome message event

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,10 @@
 var socket = io();
 
 socket.on('connect',function () {
-	console.log('connected to socket.io server');
+	console.log('connected to socket.io server!');
+});
+
+socket.on('message',function (message) {
+	console.log('new message');
+	console.log(message.text);
 });
