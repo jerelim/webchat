07407fa48f9add86afa20a77b09212e1498df7a4
jeremiah_lim <jeremiah-lim@outlook.com>
2015-12-23 16:32:54 +0800

    added user left room message

--- index.js
+++ index.js
@@ -12,6 +12,18 @@ var clientInfo={};
 io.on('connection',function (socket) {
 	console.log('user connected via socket.io');
 
+	socket.on('disconnect',function () {
+		if (typeof clientInfo[socket.id] !=='undefined') {
+			socket.leave(clientInfo[socket.id]);
+			io.to(clientInfo[socket.id].room).emit('message', {
+				name: 'System',
+				text: clientInfo[socket.id].name + ' has left the room!',
+				timestamp: moment().valueOf()
+			});
+			delete clientInfo[socket.id];
+		}
+	});
+
 	socket.on('joinRoom',function (req) {
 		clientInfo[socket.id] = req;
 		socket.join(req.room);
