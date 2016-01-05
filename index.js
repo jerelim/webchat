98792ee79e0cc773dd323cd175eafc119afb572e
jeremiah_lim <jeremiah-lim@outlook.com>
2016-01-05 16:50:19 +0800

    add @currentUsers command

--- index.js
+++ index.js
@@ -9,6 +9,30 @@ app.use(express.static(__dirname + '/public'));
 
 var clientInfo={};
 
+// sends current users to provided socket
+function sendCurrentUsers (socket) {
+	var info = clientInfo[socket.id];
+	var users = [];
+	
+	// returns if no info exists
+	if (typeof info ==='undefined') {
+		return;
+	}
+
+	Object.keys(clientInfo).forEach(function (socketId) {
+		var userInfo = clientInfo[socketId];
+		if (info.room === userInfo.room) {
+			users.push(userInfo.name);
+		}
+	});
+
+	socket.emit('message',{
+		name:'System',
+		text:'Current users: ' + users.join(', '),
+		timestamp: moment().valueOf()
+	});
+}
+
 io.on('connection',function (socket) {
 	console.log('user connected via socket.io');
 
@@ -37,8 +61,13 @@ io.on('connection',function (socket) {
 	socket.on('message',function (message) {
 		console.log('Message recieved ' + message.text);
 		// socket.broadcast.emit('message',message);
-		message.timestamp = moment().valueOf();
-		io.to(clientInfo[socket.id].room).emit('message', message);
+		if (message.text === "@currentUsers") {
+			sendCurrentUsers(socket);
+		}else{
+			// send default code
+			message.timestamp = moment().valueOf();
+			io.to(clientInfo[socket.id].room).emit('message', message);
+		}
 	});
 
 	socket.emit('message',{
